ca57fc2993fa2ad9647ef8ad03dc142b10480e03
Ryan Erricson <ryanerricson@gmail.com>
2021-05-05 14:30:47 -0400

    commit tree ffd211f8ce0d6b05d591a4e61b485e7ec2ee26d9

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books') .then(function(response) {
+    return response.json();
+  }) .then(function(json) {
+    return renderBooks(json)
+  });
+  return fetch('https://anapioficeandfire.com/api/books')
+
 }
 
 function renderBooks(books) {
